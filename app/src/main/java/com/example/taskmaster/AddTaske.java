a2c15a11690af52dea2aede350ce11004d7794c8
abdallahhamoury <abdallah.hamoury@gmail.com>
2021-11-17 15:09:46 +0200

    update

--- app/src/main/java/com/example/taskmaster/AddTaske.java
+++ app/src/main/java/com/example/taskmaster/AddTaske.java
@@ -1,12 +1,17 @@
 package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 public class AddTaske extends AppCompatActivity {
 
@@ -14,14 +19,37 @@ public class AddTaske extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_taske);
+        //get editText by id
+        EditText taskTitleInputAddPage=findViewById(R.id.titelinputAddPageInputId);
+        EditText taskBodyInputAddPage=findViewById(R.id.bodyIdInputAdd);
+        EditText taskStateInputAddPage=findViewById(R.id.stateInsideAddId);
 
+         //get add button by id
         Button addTaskButton=findViewById(R.id.buttonAdd);
         addTaskButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Toast.makeText(getApplicationContext(),"submitted",Toast.LENGTH_LONG).show();
+                ////Room///
+                //insert function//
+                //assign the user input to instance model
+                TaskClass task=new TaskClass(taskTitleInputAddPage.getText().toString(),taskBodyInputAddPage.getText().toString(),taskStateInputAddPage.getText().toString());
+                //crate database called taskDatabase
+                DatabaseTask db = Room.databaseBuilder(getApplicationContext(),
+                        DatabaseTask.class, "taskDatabase").allowMainThreadQueries().build();
+
+                TaskDao taskDao = db.taskDao();
+                //insert function from dao
+                taskDao.insertAll(task);
+
+                //try to print total////
+//                TextView taskTotalAddPage=findViewById(R.id.totalTaskAddid);
+//                List<TaskClass> getTask = taskDao.getAll();
+//                taskTotalAddPage.setText("Total task: "+getTask.size());
+
             }
         });
+
 
     }
 }
--- app/src/main/java/com/example/taskmaster/TaskDetails.java
+++ app/src/main/java/com/example/taskmaster/TaskDetails.java
@@ -1,10 +1,13 @@
 package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class TaskDetails extends AppCompatActivity {
 
@@ -17,10 +20,29 @@ public class TaskDetails extends AppCompatActivity {
 //        TextView taskTitleView =findViewById(R.id.taskTitleId);
 //        taskTitleView.setText(taskTitle);
         Intent intent=getIntent();
+        //get keys
         String taskTitle=intent.getExtras().getString("taskNameClickListener");
         String taskBody=intent.getExtras().getString("taskBodyClickListener");
         String taskState=intent.getExtras().getString("taskStateClickListener");
+
+        // get taskTitleView by id
         TextView taskTitleView =findViewById(R.id.taskTitleId);
+        TextView taskBodyView =findViewById(R.id.taskBodyDetailsPageId);
+        TextView taskStateView =findViewById(R.id.taskstatDetailsPageId);
         taskTitleView.setText("taskTitle:  "+taskTitle+" taskBody:  "+taskBody+"   taskState:  "+taskState);
+        //set text for each view
+        taskTitleView.setText("task title:  "+taskTitle);
+        taskBodyView.setText("task body:  "+taskBody);
+        taskStateView.setText("task state:  "+taskState);
+
+        // show from database this way did not work with me
+//        TaskClass taskClass;
+//        DatabaseTask db =  Room.databaseBuilder(getApplicationContext(), DatabaseTask.class, "taskDatabase").allowMainThreadQueries()
+//                .build();
+        //get doa function
+//        TaskDao taskDao = db.taskDao();
+        //store the all data from database in array
+//        taskDao.loadAllByIds(taskClass.taskId)
+
     }
 }
